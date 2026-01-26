cc9633afbb9bd6ac75b26a1d48ca9e197c6fb4f7
Noah Moorby <n.moorby@hotmail.com>
2026-01-26 20:00:59 +0000

    Add files via upload

---
+++ Can Make Arithmetic Progression From Sequence.py
@@ -0,0 +1,10 @@
+class Solution:
+    def canMakeArithmeticProgression(self, arr: List[int]) -> bool:
+        arr.sort()
+        print(arr)
+        difference = abs(arr[1] - arr[0])
+        for i in range(1, len(arr)):
+            if abs(arr[i] - arr[i-1]) != difference:
+                return False
+
+        return True
